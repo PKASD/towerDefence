df1337ddf39f0fadb4e237e119bff0af18b56b83
workout <loves1543@naver.com>
2022-08-21 23:16:35 +0900

    타워 부수면 다시 이동

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -15,17 +15,18 @@ public class Enemy : Status
     {
         timer += Time.deltaTime;
 
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.left, attackRange, LayerMask.GetMask("Center"));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.left, attackRange,
+            LayerMask.GetMask("Center", "Tower"));//센터, 타워 레이어 감지
 
         Debug.DrawRay(rigid.position, Vector3.left, new Color(0, 1, 0)); // 레이캐스트 출력
 
         if (rayHit.collider != null)
         {
-            if (rayHit.collider.CompareTag("Center") || rayHit.collider.CompareTag("tower"))
+            if (rayHit.collider.CompareTag("Center") || rayHit.collider.CompareTag("tower")) // 센터와 타워 공격
             {
                 delay = 10.0f / attackSpeed;// 공격 속도
 
-                moveSpeed = 0; // 사거리에서 멈춤
+                transform.position += new Vector3(1, 0, 0) * moveSpeed / 1000; // 사거리에서 멈춤
 
                 if (timer > delay)// 1초 후 공격
                 {
@@ -35,18 +36,18 @@ public class Enemy : Status
                 }
             }
         }
-        else {
-            Move();
-        }
+        Move();
     }
-    public void Move() {
-        transform.position += new Vector3(-1, 0, 0) * moveSpeed / 1000;//적 이동
-    }
-   /*     void OnTriggerEnter2D(Collider2D collision)
+    public void Move()
     {
-        if (collision.CompareTag("bullet"))
-        {
-            GetDamege(enemy.damege);
-        }
-    }*/
+        transform.position += new Vector3(-1, 0, 0) * moveSpeed / 1000;//적 이동
+
+    }
+    /*     void OnTriggerEnter2D(Collider2D collision)
+     {
+         if (collision.CompareTag("bullet"))
+         {
+             GetDamege(enemy.damege);
+         }
+     }*/
 }
